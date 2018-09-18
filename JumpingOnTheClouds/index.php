e5a7d2efa864f4d16d7fddcefe6b0cd69126ca1a
AymanElbery <ayman.bery@gmail.com>
2018-09-18 11:31:31 +0200

    [Fix] Jumping On The Clouds

--- JumpingOnTheClouds/index.php
+++ JumpingOnTheClouds/index.php
@@ -1,13 +1,27 @@
 <?php 
 
-$c = 8;
+$c = [0, 0, 0, 0, 1, 0];
 
 jumpingOnClouds($c);
 
 function jumpingOnClouds($c) {
 
+    $n = count($c);
+    $i = 0;
+    $counter = 0;
+    while ($i < $n-1) {
+        if ($c[$i] == 0) {
+            if ($c[$i+2] == 0) {
+                $counter++;
+                $i = $i+2;
+            }elseif ($c[$i+1] == 0) {
+                $counter++;
+                $i = $i+1;
+            }
+        }
+    }
+    return $counter;
     
-
 }
 
 
